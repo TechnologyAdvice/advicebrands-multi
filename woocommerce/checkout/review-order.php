016845dd53595e58579112468243c77cd19e1e20
Dan Brown <fireflip@gmail.com>
2018-11-26 15:55:58 -0600

    Update

--- woocommerce/checkout/review-order.php
+++ woocommerce/checkout/review-order.php
@@ -23,24 +23,23 @@ if ( ! defined( 'ABSPATH' ) ) {
 <table class="shop_table woocommerce-checkout-review-order-table">
 	<tbody>
 		<?php
-	do_action('woocommerce_review_order_before_cart_contents');
+			do_action('woocommerce_review_order_before_cart_contents');
 
-	foreach (WC()->cart->get_cart() as $cart_item_key => $cart_item) {
-		$_product = apply_filters('woocommerce_cart_item_product', $cart_item['data'], $cart_item, $cart_item_key);
+			foreach (WC()->cart->get_cart() as $cart_item_key => $cart_item) {
+				$_product = apply_filters('woocommerce_cart_item_product', $cart_item['data'], $cart_item, $cart_item_key);
 
-		if ($_product && $_product->exists() && $cart_item['quantity'] > 0 && apply_filters('woocommerce_checkout_cart_item_visible', true, $cart_item, $cart_item_key)) {
-			?>
-					<tr class="<?php echo esc_attr(apply_filters('woocommerce_cart_item_class', 'cart_item', $cart_item, $cart_item_key)); ?>">
-						<td class="product-name">
-							<?php echo apply_filters('woocommerce_cart_item_name', $_product->get_name(), $cart_item, $cart_item_key) . '&nbsp;'; ?>
-						</td>
-					</tr>
-					<?php
+				if ($_product && $_product->exists() && $cart_item['quantity'] > 0 && apply_filters('woocommerce_checkout_cart_item_visible', true, $cart_item, $cart_item_key)) {
+					?>
+							<tr class="<?php echo esc_attr(apply_filters('woocommerce_cart_item_class', 'cart_item', $cart_item, $cart_item_key)); ?>">
+								<td class="product-name">
+									<?php echo apply_filters('woocommerce_cart_item_name', $_product->get_name(), $cart_item, $cart_item_key) . '&nbsp;'; ?>
+								</td>
+							</tr>
+							<?php
 
-			}
-		}
-
-		do_action('woocommerce_review_order_after_cart_contents');
+					}
+				}
+			do_action('woocommerce_review_order_after_cart_contents');
 		?>
 	</tbody>
 </table>
