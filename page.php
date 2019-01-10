7f4cc80d21ca8a66f05ee0e96f4070dc09bcb206
Dan Brown <fireflip@gmail.com>
2019-01-10 09:43:47 -0600

    Add min height to inner content

--- page.php
+++ page.php
@@ -1,7 +1,7 @@
 <!--page-->
 <?php get_header(); ?>
 
-<div id="content">
+<div id="content" style="min-height:calc(100vh - 450px);">
 	<div id="inner-content" class="wrap row">
 		<main id="main" class="col-xs-12" role="main" itemscope itemprop="mainContentOfPage" itemtype="http://schema.org/Blog">
 			<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
